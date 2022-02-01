665fee06d5881ab8a0eb3acc223b831eda6d7133
KieraConway <kieraconway@live.com>
2022-02-01 15:06:48 -0800

    Combined work
    Combined both of our most recent updates on phase1.c and kernel.h

---
+++ Phase1/kernel.h
@@ -0,0 +1,64 @@
+#define DEBUG 1
+
+typedef struct proc_struct proc_struct;
+
+typedef struct proc_struct * proc_ptr;
+
+// ADDED FROM WEEK 3 VIDEO
+#define STATUS_EMPTY    0
+#define STATUS_RUNNING  1
+#define STATUS_READY    2
+#define STATUS_QUIT     3
+#define STATUS_BLOCKED  4
+#define STATUS_ZAPPED   5
+#define STATUS_ZAPPER   6
+#define STATUS_LAST     7
+#define SIBLING_LIST_OFFSET (sizeof(proc_ptr))
+#define ZAPPERS_LIST_OFFSET (sizeof(proc_ptr))
+
+struct proc_struct {
+	proc_ptr        next_ready_proc;        //list of ready processes [prev next_proc_ptr]
+	proc_ptr        prev_ready_proc;		// used doubly linked list
+	proc_ptr        parent_proc_ptr;		// for refering back to parent process inside of ListInsert() within phase1.c
+    proc_ptr        child_proc_ptr;         //pHead
+    proc_ptr        next_sibling_ptr;       //pNext Sibling
+    proc_ptr        prev_sibling_ptr;       //pPrev Sibling
+    proc_ptr        next_zapping_ptr;       //pNext Zapping
+    proc_ptr        prev_zapping_ptr;       //pPrev Zapping
+    //TODO
+    //proc_list     children;
+    //proc_list     zappers;
+    char            name[MAXNAME];        /* process's name */
+    char            start_arg[MAXARG];    /* args passed to process */
+    context         state;                /* current context for process */
+    short           pid;                  /* process id */
+    int             priority;
+    int (* start_func) (char *);   /* function where process begins -- launch */
+    char            *stack;
+    unsigned int    stackSize;
+    int             status;         /* READY, BLOCKED, QUIT, etc. */
+   /* other fields as needed... */
+    int             startTime;				//process start time
+    int             switchTime;				// last time switched
+};
+
+struct psr_bits {
+	unsigned int cur_mode:1;
+	unsigned int cur_int_enable:1;
+	unsigned int prev_mode:1;
+	unsigned int prev_int_enable:1;
+	unsigned int unused:28;
+};
+
+union psr_values {
+	struct psr_bits bits;
+	unsigned int integer_part;
+};
+
+/* Some useful constants.  Add more as needed... */
+#define NO_CURRENT_PROCESS NULL
+#define MINPRIORITY 5
+#define MAXPRIORITY 1
+#define SENTINELPID 1
+#define SENTINELPRIORITY LOWEST_PRIORITY
+
--- Phase1/phase1.c
+++ Phase1/phase1.c
@@ -2,8 +2,20 @@
    phase1.c
 
    CSCV 452
+<<<<<<< Updated upstream
 
+=======
+   
+	Katelyn Griffith
+	Kiera Conway
+>>>>>>> Stashed changes
    ------------------------------------------------------------------------ */
+   /* * * * * * * * * * * * * * * * * * * * *	
+   * Author Notes
+   *
+   *   for zapped processes -> use linked list
+   * * * * * * * * * * * * * * * * * * * * */
+   
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -11,13 +23,28 @@
 #include "kernel.h"
 
 /* ------------------------- Prototypes ----------------------------------- */
+<<<<<<< Updated upstream
 int sentinel (char *);        //KC TODO void to char
+=======
+int sentinel (char *);			//TODO: void-> char
+>>>>>>> Stashed changes
 extern int start1 (char *);
 void dispatcher(void);
 void launch();
 static void enableInterrupts();
 static void check_deadlock();
 
+<<<<<<< Updated upstream
+=======
+// added functions for processing kg
+static void check_kernel_mode(const char *functionName); //Check Kernel Mode
+void ClockIntHandler(int dev, void *arg);                //Clock handler
+void DebugConsole(char *format, ...);                    //Debug console
+void ListInsert(proc_ptr *child,proc_struct *table);
+int GetNextPid();
+int check_io();
+proc_ptr GetNextReadyProc();
+>>>>>>> Stashed changes
 
 /* -------------------------- Globals ------------------------------------- */
 
@@ -29,12 +56,28 @@ proc_struct ProcTable[MAXPROC];
 
 /* Process lists  */
 
+/*TODO decide process list type
+proc_ptr readyPriority1;
+proc_ptr readyPriority2;
+proc_ptr readyPriority3;
+proc_ptr readyPriority4;
+proc_ptr readyPriority5;
+*/
+
 /* current process ID */
 proc_ptr Current;
 
 /* the next pid to be assigned */
 unsigned int next_pid = SENTINELPID;
 
+<<<<<<< Updated upstream
+=======
+/* number of active processes */ 
+unsigned int numProc = 0; 	// kg
+
+/* Error Check: context_switch() Initializer */
+int init = 1;
+>>>>>>> Stashed changes
 
 /* -------------------------- Functions ----------------------------------- */
 /* ------------------------------------------------------------------------
@@ -45,7 +88,7 @@ unsigned int next_pid = SENTINELPID;
    Returns - nothing
    Side Effects - lots, starts the whole thing
    ----------------------------------------------------------------------- */
-void startup()
+   void startup()
 {
    int i;      /* loop index */
    int result; /* value returned by call to fork1() */
@@ -135,6 +178,7 @@ int fork1(char *name, int (*f)(char *), char *arg, int stacksize, int priority)
 
    /* Return if stack size is too small */
 
+<<<<<<< Updated upstream
    /* find an empty slot in the process table */
 
    /* fill-in entry in process table */
@@ -152,6 +196,46 @@ int fork1(char *name, int (*f)(char *), char *arg, int stacksize, int priority)
    }
    else
       strcpy(ProcTable[proc_slot].start_arg, arg);
+=======
+	/* find an empty slot in the process table */
+	newPid = GetNextPid();  						//get next process ID
+	proc_slot = newPid % MAXPROC;   				//assign slot
+	ProcTable[proc_slot].pid = newPid;  			//assign pid
+	ProcTable[proc_slot].priority = priority; 		//assign priority
+	ProcTable[proc_slot].status = STATUS_READY; 	//assign READY status
+	ProcTable[proc_slot].stackSize = stackSize; 	//assign stackSize
+	ProcTable[proc_slot].stack = malloc(stackSize) ;//assign stack
+
+	// Check if out of memory - malloc return value
+	if (ProcTable[proc_slot].stack == NULL)
+	{
+		DebugConsole("Out of memory.\n");
+		halt(1);
+	}
+
+	/* fill-in entry in process table */
+	if ( strlen(name) >= (MAXNAME - 1) ) {
+		DebugConsole("fork1(): Process name is too long.  Halting...\n");
+		halt(1);
+	}
+
+	//Process Name
+	strcpy(ProcTable[proc_slot].name, name);
+   
+	//Process Function
+	ProcTable[proc_slot].start_func = func;
+
+
+    //Process Function Argument(s)
+    if ( arg == NULL )
+        ProcTable[proc_slot].start_arg[0] = '\0';
+    else if ( strlen(arg) >= (MAXARG - 1) ) {
+        console("fork1(): argument too long.  Halting...\n");
+        halt(1);
+    }
+    else
+        strcpy(ProcTable[proc_slot].start_arg, arg);
+>>>>>>> Stashed changes
 
    /* Initialize context for this process, but use launch function pointer for
     * the initial value of the process's program counter (PC)
@@ -175,24 +259,34 @@ int fork1(char *name, int (*f)(char *), char *arg, int stacksize, int priority)
    ------------------------------------------------------------------------ */
 void launch()
 {
-   int result;
+	int result;
 
+<<<<<<< Updated upstream
    if (DEBUG && debugflag)
       console("launch(): started\n");
 
    /* Enable interrupts */
    //enableInterrupts();   //KC: TODO  
+=======
+	DebugConsole("launch(): started\n");
 
-   /* Call the function passed to fork1, and capture its return value */
-   result = Current->start_func(Current->start_arg);
+	/* Enable interrupts */
+	enableInterrupts();
+>>>>>>> Stashed changes
 
+	/* Call the function passed to fork1, and capture its return value */
+	result = Current->start_func(Current->start_arg);
+
+<<<<<<< Updated upstream
    if (DEBUG && debugflag)
       console("Process %d returned to launch\n", Current->pid);
+=======
+	DebugConsole("Process %d returned to launch\n", Current->pid);
+>>>>>>> Stashed changes
 
-   quit(result);
+	quit(result);
 
 } /* launch */
-
 
 /* ------------------------------------------------------------------------
    Name - join
@@ -222,8 +316,39 @@ int join(int *code)
    ------------------------------------------------------------------------ */
 void quit(int code)
 {
-   p1_quit(Current->pid);
+	p1_quit(Current->pid);
 } /* quit */
+
+/* ------------------------------------------------------------------------
+   Name - GetNextReadyProc
+   Purpose - 
+   Parameters - none
+   Returns - nothing
+   Side Effects - the context of the machine is changed
+   ----------------------------------------------------------------------- */
+proc_ptr GetNextReadyProc()
+{
+	int highestPrior = 6;
+	proc_ptr pNextProc = NULL;
+
+    /*
+     * TODO
+     * check priority lists/array
+     */
+
+	for (int i = 0; i < MAXPROC; i++)
+	{
+		// Get highest priority process running in process table
+		if ((ProcTable[i].status == STATUS_READY) && (ProcTable[i].priority < highestPrior))
+		{
+			//TODO: if (ProcTable[i].priority is < pNextProc) , remove break
+			pNextProc = &ProcTable[i];
+			break;
+	}
+	}
+
+	return pNextProc; // return pointer to next ready proc
+}
 
 
 /* ------------------------------------------------------------------------
@@ -238,9 +363,25 @@ void quit(int code)
    ----------------------------------------------------------------------- */
 void dispatcher(void)
 {
+<<<<<<< Updated upstream
    proc_ptr next_process;
 
    p1_switch(Current->pid, next_process->pid);
+=======
+	proc_ptr oldProcess; // added in kg
+	proc_ptr next_Process;
+
+	//p1_switch(Current->pid, next_process->pid); TODO
+
+	next_Process = GetNextReadyProc(); //assign newProcess; added in kg
+	oldProcess = Current; 			// added in kg
+
+	/* Make sure Current is pointing to the process we are switching to
+	must be done Before context_switch() */
+	Current = next_Process; // added in kg
+
+	context_switch((oldProcess == NULL) ? NULL : &oldProcess->state, &next_Process->state); // added in kg
+>>>>>>> Stashed changes
 } /* dispatcher */
 
 
@@ -266,19 +407,87 @@ int sentinel (char * dummy)
    }
 } /* sentinel */
 
+<<<<<<< Updated upstream
+=======
+int zap(int pid)
+{
+	// TODO 
+	//call is_zapped()
+	// won't return until zapped prcoess has called quit
+	// print error msg and halt(1) if process tries to zap itself
+	// or attempts to zap a non-existent prcoess
+
+	// return values:
+	// -1 - calling process itself was zapped while in zap
+	// 0 - zapped process has called quit
+}
+
+
+int getpid(void)
+{
+	// TODO 
+	// based on processes, check for running status and return the pid
+}
+
+
+int is_zapped(void)
+{
+	// TODO 
+	// return 0 if not zapped
+	// return 1 if zapped
+}
+
+
+void dump_processes(void)
+{
+	// TODO 
+	// prints process ifnro to console
+	// for each PCB, output:
+	// PID, parent' PID, priority, process status, # children, CPU time consumed, and name
+}
+
+>>>>>>> Stashed changes
 
 /* check to determine if deadlock has occurred... */
 static void check_deadlock()
 {
+<<<<<<< Updated upstream
+=======
+   if (check_io() == 1)
+      return;
+
+   // TODO 
+   /* Has everyone terminated? */
+   // check the number of process
+   // if there is only one active prcoess
+   // halt(0);
+   //otherwise
+   //halt(1);
+   
+>>>>>>> Stashed changes
 } /* check_deadlock */
 
+/*
+ * Enable the interrupts.
+ */
+static void enableInterrupts()
+{
+	check_kernel_mode(__func__);
+	/*Confirmed Kernel Mode*/
+	int psr = psr_get();
+
+	curPsr = curPsr | PSR_CURRENT_INT;
+
+	psr_set(curPsr); 
+
+} /* enableInterrupts */
 
 /*
  * Disables the interrupts.
  */
 void disableInterrupts()
 {
-  /* turn the interrupts OFF iff we are in kernel mode */
+  /* turn the interrupts OFF if we are in kernel mode */
   if((PSR_CURRENT_MODE & psr_get()) == 0) {
     //not in kernel mode
     console("Kernel Error: Not in kernel mode, may not disable interrupts\n");
@@ -287,3 +496,139 @@ void disableInterrupts()
     /* We ARE in kernel mode */
     psr_set( psr_get() & ~PSR_CURRENT_INT );
 } /* disableInterrupts */
+<<<<<<< Updated upstream
+=======
+
+/* ------------------------------------------------------------------------
+   Name - check_kernel_mode
+   Purpose - Checks the PSR for kernel mode and halts if in user mode
+   Parameters - functionName
+   Returns - nothing
+   Side Effects - Will halt if not kernel mode
+   ----------------------------------------------------------------------- */
+static void check_kernel_mode(const char *functionName)
+{
+   union psr_values psrValue; /* holds caller's psr values */
+
+   DebugConsole("check_kernel_node(): verifying kernel mode for %s\n", functionName);
+
+   /* test if in kernel mode; halt if in user mode */
+   psrValue.integer_part = psr_get();
+
+   if (psrValue.bits.cur_mode == 0)
+   {
+      console("Kernel mode expected, but function called in user mode.\n");
+      halt(1);
+   }
+
+   DebugConsole("Function is in Kernel mode (:\n");
+}
+
+/* ------------------------------------------------------------------------
+Name - DebugConsole
+Purpose - Prints the message to the console if in debug mode
+Parameters - format string and va args
+Returns - nothing
+Side Effects -
+----------------------------------------------------------------------- */
+void DebugConsole(char *format, ...)
+{
+	if (DEBUG && debugflag)
+	{
+		/*va_list argptr;
+		va_start(argptr, format);
+		console(format, argptr);
+		va_end(argptr);*/
+		//TODO
+		console("%s\n", format);
+	}
+}
+
+/* ------------------------------------------------------------------------
+Name - ClockIntHandler
+Purpose -
+Parameters - 
+Returns - nothing
+Side Effects -
+----------------------------------------------------------------------- */
+void clockHandler(int dev, void *arg)
+{
+	int i = 0;
+	// time-slice = 80 milliseconds
+	// detect 80 ms -> switch to next highest priority processor
+	//if (Current->runtime > 80ms) dispatcher();
+
+	sys_clock(); //returns time in microseconds
+
+	//TODO
+	// phase1notes pg 6
+	/*If current process exceeds 80ms
+		call dispatcher
+	else
+		return
+	*/
+	/*
+	* if (current->runTime > 80ms)
+	*  dispatcher();
+	*/
+
+	return;
+}
+
+/* ------------------------------------------------------------------------
+Name - GetNextPid
+Purpose - Obtain next pid whose % MAXPROC is open in the ProcTable
+Parameters - 
+Returns - NewPid or -1 if ProcTable is full
+Side Effects -
+----------------------------------------------------------------------- */
+int GetNextPid()
+{
+	int newPid = -1;
+	int procSlot = next_pid % MAXPROC;
+
+	if (numProc < MAXPROC)
+	{
+		while ((numProc < MAXPROC) && (ProcTable[procSlot].status != STATUS_EMPTY))
+		{
+			next_pid++;
+			procSlot = next_pid % MAXPROC;
+		}
+
+		newPid = next_pid++;	//assign newPid *then* increment next_pid
+	}
+
+	return newPid;
+}
+
+/* ------------------------------------------------------------------------
+ * TODO:
+Name - ListInsert
+Purpose - Add newProcess to list of child process
+Parameters -
+Returns -
+Side Effects -
+----------------------------------------------------------------------- */
+
+// Linked list - either singly or doubly
+void ListInsert(proc_ptr *child,proc_struct *table)
+{
+
+}
+
+/* ------------------------------------------------------------------------
+ * TODO:
+Name - check_io
+Purpose - 
+Parameters -
+Returns -
+Side Effects -
+----------------------------------------------------------------------- */
+// This is apparently all we need -> will be important for phase 2
+int check_io()
+{
+   return 0;
+}
+
+
+>>>>>>> Stashed changes
