09fe4fe773688d06c98b3beee3204f1b2a61b349
KieraConway <66643785+KieraConway@users.noreply.github.com>
2022-04-23 13:28:09 -0700

    Table/List Functions
    Building the structure for global process table/list functions,
    added SleepingProcs list and corresponding functions,
    Added phase4_helper.h to combine all external functions,
    updated makefile,
    and worked on finishing up sleep()

--- Phase4/lists.h
+++ Phase4/lists.h
@@ -1,4 +1,17 @@
 #ifndef PHASE_4_LISTS_H
 #define PHASE_4_LISTS_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <usloss.h>
+#include "phase4_helper.h"
+#include "driver.h"
+#include "tables.h"
+#include "process.h"
+
+#include "usloss/include/usloss.h"  //Added for Clion Functionality
+
+
 #endif //PHASE_4_LISTS_H
